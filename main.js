d652385acd1a0cf839ba9aed277a5f5dcb6eb99c
Jacob Bennett <kf7ags@gmail.com>
2018-10-25 08:24:49 -0600

    more stuff

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 
 let theImport = new Promise((resolve,reject)=>{
-    .ajax({
+    $.ajax({
         url: './file.JSON',
         type: 'GET',
         success: (response,status) => {
-            let theData = JSON.parse(response);
+            resolve(response);
+            },
+        error: (error) => {
+            reject(error);
+            }
+    });
+});
 
-        }
-    })
+theImport.then((theResponse) => {
+    let theData = JSON.parse(theResponse);
+    for (let i = 0; i < $('.card').length; i++) {
+        $('.title'[i]).html(theData.courses[i].name);
+        $('.desc'[i]).html(theData.courses[i].desc);
+    }
 });
 
